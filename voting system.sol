0e14c11592602c6e64dab63171db7da9acf98142
durgesh077 <66236446+durgesh077@users.noreply.github.com>
2022-03-29 09:39:37 +0530

    Create voting system.sol

---
+++ voting system.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+contract Vote{
+    mapping(string=>bool) public checkVote;
+    uint[] public partyVote;
+    address private owner;
+    uint public endBlock;
+    string[]public  parties;
+    constructor(string[] memory _parties){
+        parties=_parties;
+        uint numberOfParties=parties.length;
+        owner=msg.sender;
+        endBlock=block.number+uint(2 days)/13;
+        partyVote=new uint[](numberOfParties);
+    }
+    modifier is_owner() {
+        require(msg.sender==owner,"permission denied");
+        _;
+    }
+    function endCastingVote()public  is_owner returns(uint[] memory votes){
+        endBlock=block.number;
+         return partyVote;
+    }
+    function castVote(string calldata adhar,uint whom)external{
+        require(block.number<endBlock,"voting_ended");
+        require(checkVote[adhar]==false,"yo've already voted");
+        require(whom<parties.length,"invalid parity no");
+        partyVote[whom]++;
+        checkVote[adhar]=true;
+    }
+}
